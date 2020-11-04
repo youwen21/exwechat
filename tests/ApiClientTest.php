70c99b8b3dc0319229e763502c45ca696303557a
owen.bai <owen.bai@100tal.com>
2020-11-04 19:50:30 +0800

    test

---
+++ tests/ApiClientTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace youwen\exwechat\tests;
+
+use youwen\exwechat\ApiClient;
+
+class ApiClientTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testGetXmlProcessor()
+    {
+
+    }
+
+    public function testGetJsonProcessor()
+    {
+
+    }
+
+    public function testRequestHandleJson()
+    {
+
+    }
+
+    public function testRequestHandleXml()
+    {
+
+    }
+
+    public function testSend()
+    {
+
+    }
+
+    public function testGetHttpClient()
+    {
+
+    }
+
+    public function testSetHttpClient()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testSetJsonProcessor()
+    {
+
+    }
+
+    public function testSetXmlProcessor()
+    {
+
+    }
+}
---
+++ tests/ConfigTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace youwen\exwechat;
+
+
+class ConfigTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testGetSecret()
+    {
+
+    }
+
+    public function testSet()
+    {
+
+    }
+
+    public function testGetRedirectUri()
+    {
+
+    }
+
+    public function testGet()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetAppId()
+    {
+
+    }
+}

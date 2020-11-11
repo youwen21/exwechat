2aa46280dda85f95eb86b295471b600f928a7538
owen.bai <owen.bai@100tal.com>
2020-11-11 18:39:15 +0800

    图文分析

--- src/Request/UserSummary.php
+++ src/Request/UserSummary.php
@@ -13,8 +13,22 @@ class UserSummary
 {
     // 用户分析
     const GET_USER_SUMMARY = 'https://api.weixin.qq.com/datacube/getusersummary?access_token=ACCESS_TOKEN';
-    // 用户分析
     const GET_USER_CUMULATE = 'https://api.weixin.qq.com/datacube/getusercumulate?access_token=ACCESS_TOKEN';
+
+    // 图文分析 - 数据魔方
+    // 获取图文群发每日数据（getarticlesummary）
+    const GET_ARTICLE_SUMMARY = 'https://api.weixin.qq.com/datacube/getarticlesummary?access_token=ACCESS_TOKEN';
+    // 获取图文群发总数据（getarticletotal）
+    const GET_ARTICLE_TOTAL = 'https://api.weixin.qq.com/datacube/getarticletotal?access_token=ACCESS_TOKEN';
+    // 获取图文统计数据（getuserread）
+    const GET_USER_READ = 'https://api.weixin.qq.com/datacube/getuserread?access_token=ACCESS_TOKEN';
+    // 获取图文统计分时数据（getuserreadhour）
+    const GET_USER_READ_HOUR = 'https://api.weixin.qq.com/datacube/getuserreadhour?access_token=ACCESS_TOKEN';
+    // 获取图文分享转发数据（getusershare）
+    const GET_USER_SHARE = 'https://api.weixin.qq.com/datacube/getusershare?access_token=ACCESS_TOKEN';
+    // 获取图文分享转发分时数据（getusersharehour）
+    const GET_USER_SHARE_HOUR = 'https://api.weixin.qq.com/datacube/getusersharehour?access_token=ACCESS_TOKEN';
+
 
     /**
      * 获取用户增减数据
@@ -39,6 +53,20 @@ class UserSummary
     public function getUserCumulate($accessToken, $body, $headers = [])
     {
         $url = str_replace('ACCESS_TOKEN', $accessToken, self::GET_USER_CUMULATE);
+        return new Request('POST', $url, $body, $headers);
+    }
+
+    /**
+     * 图文分析 - 数据魔方
+     * @param $dataQubeUrl
+     * @param $accessToken
+     * @param $body
+     * @param array $headers
+     * @return Request
+     */
+    public function dataCube($dataQubeUrl, $accessToken, $body, $headers = [])
+    {
+        $url = str_replace('ACCESS_TOKEN', $accessToken, $dataQubeUrl);
         return new Request('POST', $url, $body, $headers);
     }
 }

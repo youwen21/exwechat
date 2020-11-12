3b2d596eb912394d44cb1aea770c223fd252324f
owen.bai <owen.bai@100tal.com>
2020-11-12 15:59:44 +0800

    加密解密消息模块

--- src/Prpcrypt.php
+++ src/Prpcrypt.php
@@ -82,76 +82,65 @@ class Prpcrypt
      */
     public function encrypt($text, $appid)
     {
-        try {
-            //获得16位随机字符串，填充到明文之前
-            $random = $this->getRandomStr();
-            $text = $random . pack("N", strlen($text)) . $text . $appid;
-            // 网络字节序
-            $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            $iv = substr($this->key, 0, 16);
-            //使用自定义的填充方式对明文进行补位填充
-            $pkc_encoder = new PKCS7Encoder;
-            $text = $pkc_encoder->encode($text);
-            mcrypt_generic_init($module, $this->key, $iv);
-            //加密
-            $encrypted = mcrypt_generic($module, $text);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
+        //获得16位随机字符串，填充到明文之前
+        $random = $this->getRandomStr();
+        $text = $random . pack("N", strlen($text)) . $text . $appid;
+        // 网络字节序
+        $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+        $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+        $iv = substr($this->key, 0, 16);
+        //使用自定义的填充方式对明文进行补位填充
+        $pkc_encoder = new PKCS7Encoder;
+        $text = $pkc_encoder->encode($text);
+        mcrypt_generic_init($module, $this->key, $iv);
+        //加密
+        $encrypted = mcrypt_generic($module, $text);
+        mcrypt_generic_deinit($module);
+        mcrypt_module_close($module);
 
-            //print(base64_encode($encrypted));
-            //使用BASE64对加密后的字符串进行编码
-            return array(ErrorCode::$OK, base64_encode($encrypted));
-        } catch (\Exception $e) {
-            //print $e;
-            return array(ErrorCode::$EncryptAESError, null);
-        }
+        //使用BASE64对加密后的字符串进行编码
+        return base64_encode($encrypted);
     }
 
     /**
      * 对密文进行解密
-     * @param string $encrypted 需要解密的密文
-     * @return string 解密得到的明文
+     * @param $encrypted string 需要解密的密文
+     * @param $appid string
+     * @return array|string 解密得到的明文
      */
     public function decrypt($encrypted, $appid)
     {
-        try {
-            //使用BASE64对需要解密的字符串进行解码
-            $ciphertext_dec = base64_decode($encrypted);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            $iv = substr($this->key, 0, 16);
-            mcrypt_generic_init($module, $this->key, $iv);
+        //使用BASE64对需要解密的字符串进行解码
+        $ciphertext_dec = base64_decode($encrypted);
+        $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+        $iv = substr($this->key, 0, 16);
+        mcrypt_generic_init($module, $this->key, $iv);
 
-            //解密
-            $decrypted = mdecrypt_generic($module, $ciphertext_dec);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
-        } catch (\Exception $e) {
-            return array(ErrorCode::$DecryptAESError, null);
+        //解密
+        $decrypted = mdecrypt_generic($module, $ciphertext_dec);
+        mcrypt_generic_deinit($module);
+        mcrypt_module_close($module);
+
+
+        //去除补位字符
+        $pkc_encoder = new PKCS7Encoder;
+        $result = $pkc_encoder->decode($decrypted);
+        //去除16位随机字符串,网络字节序和AppId
+        if (strlen($result) < 16) {
+            return false;
         }
 
-        try {
-            //去除补位字符
-            $pkc_encoder = new PKCS7Encoder;
-            $result = $pkc_encoder->decode($decrypted);
-            //去除16位随机字符串,网络字节序和AppId
-            if (strlen($result) < 16) {
-                return "";
-            }
+        $content = substr($result, 16, strlen($result));
+        $len_list = unpack("N", substr($content, 0, 4));
+        $xml_len = $len_list[1];
+        $xml_content = substr($content, 4, $xml_len);
+//        $from_appid = substr($content, $xml_len + 4);
+//
+//        if ($from_appid != $appid) {
+//            return false;
+//        }
 
-            $content = substr($result, 16, strlen($result));
-            $len_list = unpack("N", substr($content, 0, 4));
-            $xml_len = $len_list[1];
-            $xml_content = substr($content, 4, $xml_len);
-            $from_appid = substr($content, $xml_len + 4);
-        } catch (\Exception $e) {
-            //print $e;
-            return array(ErrorCode::$IllegalBuffer, null);
-        }
-        if ($from_appid != $appid) {
-            return array(ErrorCode::$ValidateAppidError, null);
-        }
-        return array(0, $xml_content);
+        return $xml_content;
     }
 
     /**
